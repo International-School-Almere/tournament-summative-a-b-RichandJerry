c958bb33399ad53cd553fdbc452f91d1496ebc65
Shiraikurokosukizzz-lgtm <Shiraikurokosukizzz@iCloud.com>
2026-04-08 11:57:31 +0200

    1

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #main file for the Tournament App.
-#test
+Players = []
+Events = []
+scores = []
+
+def add_player():
+    name = input("Add Player name")
+    Player_type   = input("Add player type(indevidual or team)")
+
+    player = {
+        "ID": len(Players),
+        "Name": name,
+        "Type": Player_type,
+        "Total_Score": 0
+    }
